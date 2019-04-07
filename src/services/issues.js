d7c6a8c0ad87d08afbea5d12ad137c411437600c
Diego Liberato <diego.liberato@outlook.com>
2019-04-07 08:36:19 -0300

    refactored variable name

--- src/services/issues.js
+++ src/services/issues.js
@@ -12,7 +12,7 @@ const findAll = async (projectId, milestone, state) => {
     const response = await fetch(`${credentials.url}/projects/${projectId}/issues?private_token=${credentials.token}&page=${currentPage}&state=${state}&milestone=${milestone}`);
     const issues = await response.json();
     const totalPages = response.headers.get('X-Total-Pages');
-    issues.forEach(e => allIssues.unshift(e));
+    issues.forEach(issue => allIssues.unshift(issue));
     morePagesAvailable = currentPage < totalPages;
   }
 
